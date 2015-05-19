04a362986e5ad7bca98837a9976a1312abbbed4e
Grant McGovern <grant@cobrain.com>
2015-05-19 15:23:05 -0400

    Adding Raspberry Pi Camera Config

---
+++ scripts/startcamera.sh
@@ -0,0 +1,1 @@
+raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
---
+++ scripts/startmjpeg.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"
