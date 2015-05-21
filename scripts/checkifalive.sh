79491bd29eebd1103ea0ddad6ae649b836fbec7b
Grant McGovern <grant@cobrain.com>
2015-05-21 00:42:36 -0400

    Adding checkifalive/restart script

---
+++ scripts/checkifalive.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+SERVICE='raspistill'
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running, everything is fine"
+else
+    echo "$SERVICE is not running"
+    sudo python sensors.py
+fi
